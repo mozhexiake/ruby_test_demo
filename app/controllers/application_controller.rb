cbd19ee9c21e2b5e127efe32ce46e42d7ba88c99
mozhexiake <rock.sang@oyohotels.cn>
2018-06-30 18:51:13 +0800

    init projects

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  
+  def hello
+    render html: "hello, world, this is a log !"
+  end
+  
+  
+end
