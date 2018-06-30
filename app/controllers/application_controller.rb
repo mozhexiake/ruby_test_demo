b92e996ac7dd335f50da636e2be7f3dc00f36acd
mozhexiake <rock.sang@oyohotels.cn>
2018-06-30 19:03:29 +0800

    修改Gemfile，引入需要模块

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   
   def hello
-    render html: "hello, world, this is a log !"
+    render html: "hello, world, this is a log 111!"
   end
   
   
