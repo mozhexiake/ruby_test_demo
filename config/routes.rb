f01564ff93ab5d67f5a96bf784a4a428be141734
mozhexiake <rock.sang@oyohotels.cn>
2018-07-01 15:03:19 +0800

    添加静态页面相关代码

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   Rails.application.routes.draw do
+
+      get  'static_pages/home'
+      get  'static_pages/help'
+      get  'static_pages/about'
+
       root 'application#hello'
     end
 
