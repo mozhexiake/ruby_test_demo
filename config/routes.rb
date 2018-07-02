3c6c43837e76c5e75428499e6867b391dcc49ced
mozhexiake <rock.sang@oyohotels.cn>
2018-07-02 08:57:26 +0800

    ruby on rails 教程demo已经学习完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # Rails.application.routes.draw do
+  #
+  #     get  'static_pages/home'
+  #     get  'static_pages/help'
+  #     get  'static_pages/about'
+  #
+  #     root 'application#hello'
+  # end
+
   Rails.application.routes.draw do
+    root 'static_pages#home'
+    get  '/home',    to: 'static_pages#home'
+    get  '/help',    to: 'static_pages#help'
+    get  '/about',   to: 'static_pages#about'
+    get  '/contact', to: 'static_pages#contact'
+  end
 
-      get  'static_pages/home'
-      get  'static_pages/help'
-      get  'static_pages/about'
-
-      root 'application#hello'
-    end
 
 end
